d7c7bd06f9a564e55878fedfa6b42d1373417df9
Satyam <satyamverma0102@gmail.com>
2024-06-09 13:37:31 +0530

    Create Propertry List

--- FrontEnd/src/app/property/property-card/property-card.component.ts
+++ FrontEnd/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'app-property-card',
@@ -6,6 +6,7 @@ import { Component } from "@angular/core";
     styleUrls: ['property-card.component.css']
 })
 export class PropertyCardComponent{
+@Input() property : any
     Property: any ={
         "Id": 1,
         "Name": "Birla House",
