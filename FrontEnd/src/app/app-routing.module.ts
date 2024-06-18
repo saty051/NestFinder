8994a78bf4941e9c4ae9ec3f0b01b54e428dd468
Satyam <satyamverma0102@gmail.com>
2024-06-18 19:23:17 +0530

    Routing-2

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 
 const routes: Routes = [
   {path: '', component: PropertyListComponent},
-  {path: 'add-property', component: AddPropertyComponent}
+  {path: 'rent-property', component: AddPropertyComponent},
+  {path: 'add-property', component: AddPropertyComponent},
+  {path: 'property-detail/:id', component: PropertyDetailComponent}
+  
 ];
 
 @NgModule({
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { PropertyListComponent } from './property/property-list/property-list.co
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { AddPropertyComponent } from './property/add-property/add-property.compo
     PropertyCardComponent,
     PropertyListComponent,
     NavBarComponent,
-    AddPropertyComponent
+    AddPropertyComponent,
+    PropertyDetailComponent
    ],
   imports: [
     BrowserModule,
