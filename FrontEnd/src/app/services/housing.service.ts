1752bec8594fb0f7ca250a9ba43d530ee0cbe40a
Satyam <satyamverma0102@gmail.com>
2024-08-13 23:35:06 +0530

    Create Property detail view

--- FrontEnd/src/app/services/housing.service.ts
+++ FrontEnd/src/app/services/housing.service.ts
@@ -12,7 +12,15 @@ export class HousingService {
 
   constructor(private http:HttpClient) { }
 
-  getAllProperties(SellRent:number): Observable<IPropertyBase[]>{
+  getProperty(id: number){
+    return this.getAllProperties().pipe(
+      map(propertiesArray => {
+        return propertiesArray.find(property => property.Id === id) as Property; 
+      })
+    );
+  }
+
+  getAllProperties(SellRent?:number): Observable<IPropertyBase[]>{
     return this.http.get<IPropertyBase[]>('assets/data/properties.json').pipe(
       map(data => {
         const propertiesArray: Array<IPropertyBase> = [];
@@ -20,17 +28,27 @@ export class HousingService {
         const localProperties = JSON.parse(localStorage.getItem('newProp') || '{}');
         if(localProperties){
           for(const id in localProperties){
-            if(localProperties.hasOwnProperty(id)&& localProperties[id].SellRent === SellRent){
+            if(SellRent){
+              if(localProperties.hasOwnProperty(id)&& localProperties[id].SellRent === SellRent){
                 propertiesArray.push(localProperties[id]);
               }
+            }else{
+              propertiesArray.push(localProperties[id]);
+            }
+            
             }
         }
 
         for(const id in data){
+          if(SellRent){
             if(data.hasOwnProperty(id)&& data[id].SellRent === SellRent){
-                propertiesArray.push(data[id]);
-              }
+              propertiesArray.push(data[id]);
             }
+          }
+          else{
+            propertiesArray.push(data[id]);
+          }
+        }
         return propertiesArray;
       }
     )
