3c087ba28c6f124ba12777cf6c26f00030e707e0
Satyam <satyamverma0102@gmail.com>
2024-08-22 12:59:49 +0530

    Perform Add and Delete operation using Entity Framework  Core

--- BackEnd/WebAPI/Controllers/CityController.cs
+++ BackEnd/WebAPI/Controllers/CityController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using WebAPI.Data;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -14,12 +17,47 @@ namespace WebAPI.Controllers
         }
 
 
-
+        // GET api/City
         [HttpGet]
-        public IActionResult GetCities()
+        public async Task<IActionResult> GetCities()
         {
-            var cities = _dataContext.Cities.ToList();
+            var cities = await _dataContext.Cities.ToListAsync();
             return Ok(cities);
         }
+
+        // Post api/City/add?cityName=Miami
+        // Post api/City/add/Los Angeles
+
+        [HttpPost("Add")]
+        [HttpPost("Add/{cityName}")]
+        public async Task<IActionResult> AddCity(string cityName)
+        {
+            City city = new City();
+            city.Name = cityName;
+            await _dataContext.Cities.AddAsync(city);
+            await _dataContext.SaveChangesAsync();
+            return Ok(city);
+        }
+
+        // Post api/City/post/Los Angeles  --Post the data in JSON Format
+        [HttpPost("post")]
+        public async Task<IActionResult> AddCity(City city)
+        {
+            //City city = new City();
+            //city.Name = cityName;
+            await _dataContext.Cities.AddAsync(city);
+            await _dataContext.SaveChangesAsync();
+            return Ok(city);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            var city = await _dataContext.Cities.FindAsync(id);
+            _dataContext.Cities.Remove(city);
+            await _dataContext.SaveChangesAsync();
+            return Ok(city);
+        }
+
     }
 }
--- BackEnd/WebAPI/Program.cs
+++ BackEnd/WebAPI/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Cors;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WebAPI.Data;
+using WebAPI.Repo;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +23,7 @@ builder.Services.AddCors(options =>
                 .AllowAnyMethod()
                 .AllowAnyHeader());
 });
+builder.Services.AddScoped<ICityRepository, CityRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
