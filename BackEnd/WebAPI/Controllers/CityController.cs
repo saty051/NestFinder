97975a2a48f406879d94a1d2b5590fceb70b6943
Satyam <satyamverma0102@gmail.com>
2024-08-22 15:54:59 +0530

    Removing .gitignore file

---
+++ BackEnd/WebAPI/Controllers/CityController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Repo;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        public readonly ICityRepository _repo;
+        public CityController(ICityRepository repo) {
+            _repo = repo;
+        }
+
+
+        // GET api/City
+        [HttpGet]
+        public async Task<IActionResult> GetCities()
+        {
+            var cities = await _repo.GetCitiesAsync();
+            return Ok(cities);
+        }
+
+        // Post api/City/add?cityName=Miami
+        // Post api/City/add/Los Angeles
+
+        //[HttpPost("Add")]
+        //[HttpPost("Add/{cityName}")]
+        //public async Task<IActionResult> AddCity(string cityName)
+        //{
+        //    City city = new City();
+        //    city.Name = cityName;
+        //    await _dataContext.Cities.AddAsync(city);
+        //    await _dataContext.SaveChangesAsync();
+        //    return Ok(city);
+        //}
+
+        // Post api/City/post/Los Angeles  --Post the data in JSON Format
+        [HttpPost("post")]
+        public async Task<IActionResult> AddCity(City city)
+        {
+            _repo.AddCity(city);
+            await _repo.SaveAsync();
+            return StatusCode(201);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            _repo.DeleteCity(id);
+            await _repo.SaveAsync();
+            return Ok(id);
+        }
+
+    }
+}
---
+++ BackEnd/WebAPI/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Cors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAPI.Data;
+using WebAPI.Repo;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Enable globalization support is fully enabled
+AppContext.SetSwitch("System.Globalization.Invariant", false);
+
+// Register the DbContext
+builder.Services.AddDbContext<DataContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",policy =>
+        policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+});
+builder.Services.AddScoped<ICityRepository, CityRepository>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
